55a4ccd88f7ba306796bcc7fdea5e76824687a36
Yuri <yuri.khristoforov@gmail.com>
2026-05-22 15:10:05 +0300

    test 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 // запускаем сервер
+// это тестовая строчка
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
